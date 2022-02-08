e63ff135c83367c0b51446b307c15cb6b18359c9
Stojan Kadiev <kadievstojan@gmail.com>
2022-02-08 11:26:12 +0100

    controlers with views for login/register

---
+++ app/controllers/LoginController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Core\App;
+
+class LoginController {
+   
+    public function index() {
+       
+        require_once APP_ROOT . '/app/views/login/index.php';
+
+    }
+
+    
+   
+}
---
+++ app/controllers/RegisterController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Core\App;
+
+class RegisterController {
+   
+    public function index() {
+       
+        require_once APP_ROOT . '/app/views/register/index.php';
+
+    }
+
+    
+   
+}
